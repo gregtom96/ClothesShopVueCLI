d9af1bfa9fe253d4d864087934ae021e0ad0f529
gregtom96 <gregzi96@gmail.com>
2024-03-30 10:06:34 -0300

    récupération du montant total du panier dans le bouton paypal

--- src/components/PaypalButtons.vue
+++ src/components/PaypalButtons.vue
@@ -1,36 +1,56 @@
-<script setup>
-import { loadScript } from '@paypal/paypal-js'
-
-// Change to your CLIENT ID gotten from the developer dashboard
-const CLIENT_ID = 'AVZOKK4SJyLcHxpBrhdACR9N-zZZ9KwTiUReMZsUcgibeqefK-HJq-oMDY3S0NsBv0qvWiO0AW2r77V7'
-
-loadScript({ 'client-id': CLIENT_ID }).then((paypal) => {
-    paypal.Buttons({
-        createOrder: function(data, actions){
-            return actions.order.create({
-                purchase_units: [{
-                    amount: {
-                        value: '10'
-                    }
-                }]
-            })
-        },
-        onApprove: function(data, actions){
-            return actions.order.capture().then(function(details){
-                alert("Transaction OK : "+details.payer.name.given_name);
-            })
-        },
-        onError: function (err){
-            console.error('Payment error :', err);
-            alert("Payment failed !");
-        }
-    }).render("#paypal-button-container");
-})
-</script>
-
 <template>
     <div id="paypal-button-container"></div> 
-</template>
+</template> 
+
+<script setup>
+import { loadScript } from '@paypal/paypal-js'
+import { useStore } from 'vuex'
+import { ref, onMounted } from 'vue'
+
+// Change to your CLIENT ID gotten from the developer dashboard
+const CLIENT_ID = 'AfMIDAWqa_pSP2k1YSFKAVTcf2OfgSa5XFCNddL_YGysLum6lzJjS3CTbrCBiA5l9_br9AB3fr7DyKJH'
+/*Client ID Live : AVZOKK4SJyLcHxpBrhdACR9N-zZZ9KwTiUReMZsUcgibeqefK-HJq-oMDY3S0NsBv0qvWiO0AW2r77V7
+Client ID Sandbox : AfMIDAWqa_pSP2k1YSFKAVTcf2OfgSa5XFCNddL_YGysLum6lzJjS3CTbrCBiA5l9_br9AB3fr7DyKJH
+Sandbox account email : sb-6varj29523569@personal.example.com
+Sandbox account password : g&I%LBX7*/
+
+const totalPrice = ref(0)
+
+// Retrieve total price from Vuex store
+const store = useStore()
+const getTotalPrice = () => store.getters.getTotalPrice
+
+onMounted(() => {
+  // Load PayPal script
+  loadScript({ 'client-id': CLIENT_ID }).then((paypal) => {
+    paypal.Buttons({
+      createOrder: function(data, actions){
+        return actions.order.create({
+          purchase_units: [{
+            amount: {
+              value: totalPrice.value.toString()
+            }
+          }]
+        })
+      },
+      onApprove: function(data, actions){
+        return actions.order.capture().then(function(details){
+          alert("Transaction OK : "+details.payer.name.given_name);
+        })
+      },
+      onError: function (err){
+        console.error('Payment error :', err);
+        alert("Payment failed !");
+      }
+    }).render("#paypal-button-container");
+  })
+})
+
+// Update total price when component is mounted
+onMounted(() => {
+    totalPrice.value = getTotalPrice()
+})
+</script>
 
 <style scoped>
 #paypal-button-container {
