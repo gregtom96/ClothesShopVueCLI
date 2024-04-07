11e3ba3bd29ade4ca358269c567341bbf1e67dc8
gregtom96 <gregzi96@gmail.com>
2024-04-07 17:11:06 -0300

    ajout header sur page article

--- src/components/ArticleComponent.vue
+++ src/components/ArticleComponent.vue
@@ -1,16 +1,42 @@
 <template>
-    <h1>Article page</h1>
-    <a href="../../documents/CV.pdf" download> Download the article </a>
+    <header>
+        <router-link to="/">
+            <h1 @click="resetPanier()">Tee Shirt Shop</h1>
+        </router-link>
+    </header>
+    <p>
+        <a href="../../documents/CV.pdf" download> Download the article </a>
+    </p>
 </template>
 
 <script>
 export default{
-    name:'ArticleComponent'
+    name:'ArticleComponent',
+    methods: {
+        resetPanier(){
+            this.$store.commit('SET_ARTICLE_PRICE', 0)
+            this.$store.commit('RESET_TOTAL_PRICE')
+        }
+    }
 }
 </script>
 
 <style scoped>
+header{
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    background-color: black;
+    padding-left: 10px;
+    padding-right: 10px;
+    margin-bottom: 10px;
+    height: 60px;
+}
 h1{
+    font-family: "Delicious Handrawn";
+    color: white;
+}
+p{
     text-align: center;
 }
 </style>
